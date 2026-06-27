646ee6c19fd1381335186bdfd48acefd0857b9cf
Zaeronis <hunter.dalton81396@gmail.com>
2026-06-27 15:00:17 -0500

    Refactor and redesign landing page components; update styles, and introduce reusable `Card` and `Excerpt` components.
    Signed-off-by: Zaeronis <hunter.dalton81396@gmail.com>

--- PaxEvangelium/src/App.jsx
+++ PaxEvangelium/src/App.jsx
@@ -1,25 +1,11 @@
 import './App.css'
-import EphesiansSix from "./assets/pages/landing/com/EphesiansSix.jsx";
+import Landing from "./assets/pages/landing/Landing.jsx";
 
 function App() {
 
   return (
     <>
-      <div>
-        <img
-            src="/pax.png"
-            alt="Pax Evangelium Logo"
-            className="logo"
-        />
-        <h1 className="title">Pax Evangelium</h1>
-        <br/>
-        <br/>
-        <EphesiansSix />
-        <br/>
-        <br/>
-        <p>An all-in-one platform for spiritual utility and community.</p>
-        <p><em>Coming Soon.</em></p>
-      </div>
+        <Landing />
     </>
   )
 }

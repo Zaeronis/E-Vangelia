e01fece9a0782d481faf79924b1414ec2215e47a
Zaeronis <hunter.dalton81396@gmail.com>
2026-06-28 12:40:24 -0500

    Remove all project files and dependencies related to Pax Evangelium, including configuration, components, styles, and assets.
    Signed-off-by: Zaeronis <hunter.dalton81396@gmail.com>

---
+++ E-Vangelia/src/assets/global/com/Card.jsx
@@ -0,0 +1,11 @@
+import '../styles/Card.css'
+
+export default function Card({children}) {
+    return (
+        <div
+            className="card"
+        >
+            {children}
+        </div>
+    )
+}
